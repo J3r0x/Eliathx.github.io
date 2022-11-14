988f36218640bc5467c6de200a6701e722a8ecda
Jer0x <94476145+J3r0x@users.noreply.github.com>
2022-11-13 20:58:59 -0500

    About Changed

--- imagen-sistemas/src/pages/quienesSomos.js
+++ imagen-sistemas/src/pages/quienesSomos.js
@@ -1,46 +1,62 @@
 import "../styles/quienesSomos.css"
 
 function QuienesSomos() {
-    return <div className="about-div page" style={{textAlign: "center"}}>
-        <h1 className={"big-title"}>Quiénes somo</h1>
+    return<div className=" page">
+        <div className="aboutBG">
+            <h1 className="aboutTittle">Quiénes somos</h1>
+        </div>
+        <div className="about-div ">
+            <div className="aboutBox">
+                <div className="aboutItems">
+                    <h2 className="aboutCenter">Acerca de nuestro Trabajo</h2>
+                    <div className="Description">
+                        <div>Imagen Sistemas se encarga de promover la imagen de la Facultad de Ingeniería de Sistemas a través de la realización de diferentes actividades como:  </div>
+                    </div>
 
-        <h2 className={"center-title"}>Descripcion</h2>
-        <div>
-            <div style={{textAlign: "center"}}>Imagen Sistemas se encarga de promover la imagen de la Facultad de Ingeniería de Sistemas a través de la realización de diferentes actividades como:  </div>
+                    <div className= "activityBox">
+                        <ActivityCard
+                            img="https://www.unir.net/wp-content/uploads/2021/04/210419_img_ficha_web.jpg"
+                            description = {"Coordinación para participación de la FIS en jornadas y concursos organizados por otras universidades e instituciones."}/>
+                        <ActivityCard
+                            img="https://cdn.discordapp.com/attachments/925928236481912839/1041473158408056943/Captura.PNG"
+                            description = {"Organización de cursos, conferencias y casas abiertas de la FIS."}/>
+                        <ActivityCard
+                            description = {"Coordinación y organización de giras técnicas."}/>
+                        <ActivityCard
+                            description = {"Soporte y mantenimiento a escuelas y colegios."}/>
+                        <ActivityCard
+                            description = {"Gestión de carteleras y publicidad gráfica de la imagen de la FIS."}/>
+                    </div>
 
-            <div >
-                <ActivityCard description = {"Coordinación para participación de la FIS en jornadas y concursos organizados por otras universidades e instituciones."}/>
-                <ActivityCard description = {"Organización de cursos, conferencias y casas abiertas de la FIS."}/>
-                <ActivityCard description = {"Coordinación y organización de giras técnicas."}/>
-                <ActivityCard description = {"Soporte y mantenimiento a escuelas y colegios."}/>
-                <ActivityCard description = {"Gestión de carteleras y publicidad gráfica de la imagen de la FIS."}/>
+                    <div className="Mision">
+                        <h2>Misión</h2>
+                        <p>Imagen Sistemas se encarga de promover la imagen de la Facultad de
+                    Ingeniería de Sistemas a través de la realización de diferentes actividades que
+                    permitan mostrar el conocimiento y aplicación de las Tecnologías de Información y
+                    Comunicaciones para solucionar problemas de transcendencia para la sociedad.</p>
+                    </div>
+
+                    <div>
+                        <h2>Visión</h2>
+                        <p>Imagen Sistemas pretende posicionar a La Facultad de Ingeniería de Sistemas
+                    como un referente de la Escuela Politécnica Nacional en el campo de las Tecnologías
+                    de la Información y Comunicaciones.</p>
+                        <h2>Valores</h2>
+                    </div>
+                    <div>
+                        <span>Responsabilidad | Trabajo en equipo | Compromiso | Innovación | Calidad</span>
+                    </div>
+                </div>
             </div>
-
-        </div>
-
-        <div>
-            <h2 className={"center-title"}>Misión</h2>
-            <span>Imagen Sistemas se encarga de promover la imagen de la Facultad de
-            Ingeniería de Sistemas a través de la realización de diferentes actividades que
-            permitan mostrar el conocimiento y aplicación de las Tecnologías de Información y
-            Comunicaciones para solucionar problemas de transcendencia para la sociedad.</span>
-        </div>
-
-        <div>
-            <h2 className={"center-title"}>Visión</h2>
-            <span>Imagen Sistemas pretende posicionar a La Facultad de Ingeniería de Sistemas
-            como un referente de la Escuela Politécnica Nacional en el campo de las Tecnologías
-            de la Información y Comunicaciones.</span>
-            <h2 className={"center-title"}>Valores</h2>
-        </div>
-        <div style={{textAlign: "center"}}>
-            <span>Responsabilidad | Trabajo en equipo | Compromiso | Innovación | Calidad</span>
         </div>
     </div>
 }
 
 const ActivityCard = (props) => {
     return <div className={"activity-card"}>
+        <div className={"activity-card-img-wrapper"}>
+            <img className={"activity-card-img"} src={props.img}></img>
+        </div>
         <div className="activity-card-content">
             <a>{props.description}</a>
         </div>
